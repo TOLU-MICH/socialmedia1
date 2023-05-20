f8616388f11c10c8fa3b80e74ffa539a7e9a15b2
TOLU-MICH <michealtolu2003@gmail.com>
2023-05-20 11:28:30 +0100

    Defined the right side bar component.

---
+++ src/RightSideBar.js
@@ -0,0 +1,30 @@
+import { Card, CardHeader, CardMedia, IconButton, Stack } from "@mui/material";
+import React from "react";
+import Friends from "./friends";
+import { Users } from "./dummyData";
+
+export default function RightSideBar() {
+  return (
+    <div className="right-side--bar">
+      <Card sx={{ boxShadow: "none" }}>
+        <CardHeader
+          avatar={
+            <IconButton>
+              <img src="assets/gift.png" alt="" className="ads-image" />
+            </IconButton>
+          }
+          title={
+            <p>
+              <strong>Pola Foster</strong> and <strong>3 other friend</strong>{" "}
+              have a birthday today
+            </p>
+          }
+          sx={{ padding: 0 }}
+        />
+
+        <CardMedia component="img" height="266" image="assets/ad.png" alt="" />
+      </Card>
+      <Friends data={Users} online={true} />
+    </div>
+  );
+}
