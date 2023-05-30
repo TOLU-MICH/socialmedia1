5e2fa1db4dc321829b301a5c34ef0031eb990679
TOLU-MICH <michealtolu2003@gmail.com>
2023-05-31 00:15:32 +0100

    Defined the login/register page.

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,32 @@ import { Stack } from "@mui/material";
 import Feed from "./feed";
 import BackdropImage from "./BackdropImage";
 import RightProfileBar from "./RightProfileBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
-    <>
-      <TopNavBar />
-      <main className="content-container">
-        <LeftSideBar />
-        <Stack spacing={3}>
-          <BackdropImage />
-          <Stack direction="row">
-            <div className="content-child">
-              <Share />
-              <Feed data={Posts} Users={Users} />
-            </div>
-            <RightProfileBar data={Users} />
-          </Stack>
-        </Stack>
-      </main>
-    </>
+    <Router>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </Router>
+    // <>
+    //   <TopNavBar />
+    //   <main className="content-container">
+    //     <LeftSideBar />
+    //     <Stack spacing={3}>
+    //       <BackdropImage />
+    //       <Stack direction="row">
+    //         <div className="content-child">
+    //           <Share />
+    //           <Feed data={Posts} Users={Users} />
+    //         </div>
+    //         <RightProfileBar data={Users} />
+    //       </Stack>
+    //     </Stack>
+    //   </main>
+    // </>
   );
 }
-
-
--- src/RightProfileBar.jsx
+++ src/RightProfileBar.jsx
@@ -2,17 +2,17 @@ export default function RightProfileBar({ data }) {
   return (
     <div className="right-side--profile__bar">
       <section>
-          <h3>User Information</h3>
-          <span className="content-container">
-            City: <p className="city">New York</p>
-          </span>
-          <span className="content-container">
-            From:<p className="from">Madrid</p>
-          </span>
-          <span className="content-container">
-            Relationship:<p className="relationship">Single</p>
-          </span>
-          <h3>User Friends</h3>
+        <h3>User Information</h3>
+        <span className="content-container">
+          City: <p className="city">New York</p>
+        </span>
+        <span className="content-container">
+          From:<p className="from">Madrid</p>
+        </span>
+        <span className="content-container">
+          Relationship:<p className="relationship">Single</p>
+        </span>
+        <h3>User Friends</h3>
       </section>
       <section className="profile-friends">
         {data.map((user) => {
@@ -20,7 +20,7 @@ export default function RightProfileBar({ data }) {
             <div className="profile-friend">
               <img
                 src={user.profilePicture}
-                alt="profile picture"
+                alt=""
                 className="profile-friend--img"
               />
               <p>{user.username}</p>
